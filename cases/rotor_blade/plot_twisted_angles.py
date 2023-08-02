67f864f821d36701b9853397f763c1ad2df7eb5c
kit71717 <calvin9696@hotmail.com>
2023-08-01 22:30:39 -0400

    Latest working with Rotor

--- cases/rotor_blade/plot_twisted_angles.py
+++ cases/rotor_blade/plot_twisted_angles.py
@@ -18,23 +18,27 @@ filename="blade_geo.dat"
 x,y = np.loadtxt(filename,usecols=(3,1),unpack=True)
 x_store.append(x)
 y_store.append(y)
-labels_store.append("Iter 1")
+labels_store.append("Design Cycle 1")
 # set 2
 x,y = np.loadtxt(filename,usecols=(3,4),unpack=True)
 x_store.append(x)
 y_store.append(y)
-labels_store.append("Iter 2")
+labels_store.append("Design Cycle 2")
 # set 3
 x,y = np.loadtxt(filename,usecols=(3,5),unpack=True)
 x_store.append(x)
 y_store.append(y)
-labels_store.append("Iter 3")
+labels_store.append("Design Cycle 3")
 
 qp.plotfxn(x_store,y_store,
     figure_filename="twisted_angles",
-    figure_size=(6,6),
+    figure_size=(9,6),
     legend_labels_tex=labels_store,
     figure_filetype="pdf",
-    title_label="Spanwise Postion from Tip to Root vs Twisted Angles",
-    ylabel="Twisted Angle [$\\circ$]",
-    xlabel="Spanwise Postion [mm]")
+    title_label="Spanwise Position from Tip to Root vs Twisted Angle",
+    ylabel="Twisted Angle [$^{\\circ}$]",
+    xlabel="Spanwise Position [mm]",
+    xlimits=[0,230],
+    transparent_legend=False,
+    legend_border_on=True,
+    grid_lines_on=False)
